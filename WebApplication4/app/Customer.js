88f663e93e8d1c70a1d9b5a999594b1fe17321fd
Arthur Geng <arthur_geng2008@windowslive.com>
2017-04-11 00:38:46 +0300

    Add project files.

---
+++ WebApplication4/app/Customer.js
@@ -0,0 +1,11 @@
+"use strict";
+var Customer = (function () {
+    function Customer() {
+        this.CustomerName = "asdsad";
+        this.Code = "asdsa";
+        this.Amount = "asdasd";
+    }
+    return Customer;
+}());
+exports.Customer = Customer;
+//# sourceMappingURL=Customer.js.map
